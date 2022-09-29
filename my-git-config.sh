525e9a2ef027e5824d3b4216d7af2067f0b7ebd8
Eric Golan <golan@harel.co.il>
2022-09-29 05:49:33 -0400

    Finalzing my-git-config.sh file !

--- my-git-config.sh
+++ my-git-config.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# test
-# test 2
-# test 3
-# test 4
-
-# test 5
-# test 6
-# test 5
 
 git config --global --add user.name "Eric Golan"
 git config --global --add user.email "golan@harel.co.il"
