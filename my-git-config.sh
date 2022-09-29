00234109cb8811b1ac9a3cd7b4792f63a78acd74
Eric Golan <golan@harel.co.il>
2022-09-29 05:56:19 -0400

    still not 100% finished

--- my-git-config.sh
+++ my-git-config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # updaing the file ~/.gitconfig
-git config --global --add user.name "Eric Golan"
-git config --global --add user.email "golan@harel.co.il"
+# note that --add will create more of the same, remove --add to change existing
+# git config --global --add user.name "Eric Golan"
+git config --global "Eric Golan"
+# git config --global --add user.email "golan@harel.co.il"
+git config --global "golan@harel.co.il"
